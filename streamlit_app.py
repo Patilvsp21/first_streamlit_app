9f142b30807ff5ef948af5231566aa65d627f1e3
Patilvsp21 <130983015+Patilvsp21@users.noreply.github.com>
2023-04-25 23:01:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "fruit_choice" )
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice )
 #streamlit.text(fruityvice_response.json()) # write data to screen 
 
 # take json version of response and normalize it 
